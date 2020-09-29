102d129a5b6b3f49a397f80ce02f1474a3c55ea1
TimaJuma <tima.xpl@gmail.com>
2020-09-29 01:41:06 -0600

    resolve

--- react-front-end/src/layouts/Chat/MessageInput.jsx
+++ react-front-end/src/layouts/Chat/MessageInput.jsx
@@ -10,9 +10,11 @@ const MessageInput = ({ setMessage, sendMessage, message }) => (
       placeholder="Type a message..."
       value={message}
       onChange={({ target: { value } }) => setMessage(value)}
-      onKeyPress={event => (event.key === "Enter" ? sendMessage(event) : null)}
+      onKeyPress={(event) =>
+        event.key === "Enter" ? sendMessage(event) : null
+      }
     />
-    <button className="sendButton" onClick={e => sendMessage(e)}>
+    <button className="sendButton" onClick={(e) => sendMessage(e)}>
       Send
     </button>
   </form>
