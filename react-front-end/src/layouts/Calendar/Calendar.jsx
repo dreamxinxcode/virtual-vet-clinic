72dbdfed384037da479a40d7c1718c19304bad60
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-09-29 19:55:26 -0700

    Styling to appointments slots

--- react-front-end/src/layouts/Calendar/Calendar.jsx
+++ react-front-end/src/layouts/Calendar/Calendar.jsx
@@ -1,25 +1,21 @@
 import React, { useState } from "react";
 import Appointment from "../Calendar/Appoinments";
+import Calendar from "react-calendar";
 
 import Chat from "../Chat/Chat";
 
-import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./Calendar.scss";
 
 export default function myCalendar() {
-  const [value, setDate] = useState(new Date());
-
-  const dateHander = () => {
-    setDate(value);
-    console.log(value);
-  };
+  const [value, onChange] = useState(new Date());
+  console.log(value);
 
   return (
-    <>
-      <Calendar onChange={dateHander} value={value} />
+    <div>
+      <Calendar onChange={onChange} value={value} />
       <Appointment />
       <Chat />
-    </>
+    </div>
   );
 }
