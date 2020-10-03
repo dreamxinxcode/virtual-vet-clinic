3d57caf612743022c6f1163194059207317de8de
TimaJuma <tima.xpl@gmail.com>
2020-10-03 15:18:08 -0600

    Cleaning of DB file

--- express-back-end/database.js
+++ express-back-end/database.js
@@ -1,5 +1,4 @@
-// const properties = require('./json/properties.json');
-// const users = require('./json/users.json');
+// CREATE POOL
 const { Pool } = require("pg");
 
 const pool = new Pool({
@@ -11,18 +10,7 @@ const pool = new Pool({
 
 exports.pool = pool;
 
-const getNames = () => {
-  return pool
-    .query(
-    `
-      SELECT * FROM names;
-    `
-    )
-    .then(res => res.rows)
-    .catch(err => console.error(err));
-};
-exports.getNames = getNames;
-
+// 1
 const getUserWithEmail = (email, type) => {
   let account_type;
   console.log("DB query with EMAIl & TYPE", email, type);
@@ -47,14 +35,41 @@ const getUserWithEmail = (email, type) => {
       console.log(res.rows[0]);
       return res.rows[0];
     })
-    .catch(err => {
+    .catch((err) => {
       console.error("query error", err.stack);
       return null;
     });
 };
 exports.getUserWithEmail = getUserWithEmail;
 
-const getClinic = options => {
+// 2
+const getUserWithId = (id, type) => {
+  let account_type;
+  console.log("DB query with ID, and type", id, type);
+  if (type === "pet") {
+    console.log("PET TYPE recieved");
+    account_type = "owners";
+  } else if (type === "clinic") {
+    console.log(" CLINIC TYPE recieved");
+    account_type = "clinics";
+  }
+  return pool
+    .query(
+      `    SELECT *    FROM ${account_type}    WHERE id = $1    LIMIT 1;    `,
+      [id]
+    )
+    .then((res) => {
+      return res.rows[0];
+    })
+    .catch((err) => {
+      console.error("query error", err.stack);
+      return null;
+    });
+};
+exports.getUserWithId = getUserWithId;
+
+// 3
+const getClinic = (options) => {
   const queryParams = [];
   let queryString = `
   SELECT DISTINCT clinics.* FROM pet_types
@@ -90,61 +105,42 @@ const getClinic = options => {
 
   return pool
     .query(queryString, queryParams)
-    .then(res => {
-      return res.rows;
-    })
-    .catch(err => {
-
-const getUserWithId = (id, type) => {
-  let account_type;
-  console.log("DB query with ID, and type", id, type);
-  if (type === "pet") {
-    console.log("PET TYPE recieved");
-    account_type = "owners";
-  } else if (type === "clinic") {
-    console.log(" CLINIC TYPE recieved");
-    account_type = "clinics";
-  }
-  return pool
-    .query(
-      `    SELECT *    FROM ${account_type}    WHERE id = $1    LIMIT 1;    `,
-      [id]
-    )
     .then((res) => {
-      return res.rows[0];
+      return res.rows;
     })
     .catch((err) => {
       console.error("query error", err.stack);
       return null;
     });
 };
-exports.getUserWithId = getUserWithId;
+exports.getClinic = getClinic;
 
+// 4
 const getUserAppointments = (userID, type) => {
   if (type === "clinic") {
-    console.log('clinic ')
+    console.log("clinic ");
     return pool
-    .query(
-      `
+      .query(
+        `
     SELECT * 
     FROM appointments
     WHERE clinic_id = $1
     `,
-      [userID]
-    )
-    .then((res) => {
-      return res.rows;
-    })
-    .catch((err) => {
-      console.error("query error", err.stack);
-      return null;
-    });
+        [userID]
+      )
+      .then((res) => {
+        return res.rows;
+      })
+      .catch((err) => {
+        console.error("query error", err.stack);
+        return null;
+      });
   }
 
-  console.log('owner')
+  console.log("owner");
   return pool
     .query(
-    `
+      `
     SELECT clinics.name as clinic_name, pets.name as pet_name, appointments.date_apt as appointment_date, appointments.time_id as appointment_time
     FROM appointments
     JOIN clinics ON clinics.id = appointments.clinic_id
@@ -155,7 +151,7 @@ const getUserAppointments = (userID, type) => {
       [userID]
     )
     .then((res) => {
-      console.log(res.rows)
+      console.log(res.rows);
       return res.rows;
     })
     .catch((err) => {
@@ -163,5 +159,4 @@ const getUserAppointments = (userID, type) => {
       return null;
     });
 };
-exports.getClinic = getClinic;
 exports.getUserAppointments = getUserAppointments;
