39d74166eafbbcabe09622c14485ea6c38c4ee38
TimaJuma <tima.xpl@gmail.com>
2020-10-02 22:54:39 -0600

    BE

--- express-back-end/database.js
+++ express-back-end/database.js
@@ -24,17 +24,50 @@ const getNames = () => {
 exports.getNames = getNames;
 
 const getUserWithEmail = (email, type) => {
-  if (type === "pet") console.log("PET TYPE recieved");
-  if (type === "clinic") console.log(" CLINIC TYPE recieved");
+  let account_type;
+  console.log("DB query with EMAIl & TYPE", email, type);
+  if (type === "pet") {
+    console.log("PET TYPE recieved");
+    account_type = "owners";
+  } else if (type === "clinic") {
+    console.log(" CLINIC TYPE recieved");
+    account_type = "clinics";
+  }
   return pool
     .query(
       `
     SELECT *
-    FROM users
+    FROM ${account_type}
     WHERE email = $1
     LIMIT 1;
     `,
       [email]
+    )
+    .then((res) => {
+      console.log(res.rows[0]);
+      return res.rows[0];
+    })
+    .catch((err) => {
+      console.error("query error", err.stack);
+      return null;
+    });
+};
+exports.getUserWithEmail = getUserWithEmail;
+
+const getUserWithId = (id, type) => {
+  let account_type;
+  console.log("DB query with ID, and type", id, type);
+  if (type === "pet") {
+    console.log("PET TYPE recieved");
+    account_type = "owners";
+  } else if (type === "clinic") {
+    console.log(" CLINIC TYPE recieved");
+    account_type = "clinics";
+  }
+  return pool
+    .query(
+      `    SELECT *    FROM ${account_type}    WHERE id = $1    LIMIT 1;    `,
+      [id]
     )
     .then((res) => {
       return res.rows[0];
@@ -44,4 +77,4 @@ const getUserWithEmail = (email, type) => {
       return null;
     });
 };
-exports.getUserWithEmail = getUserWithEmail;
+exports.getUserWithId = getUserWithId;
