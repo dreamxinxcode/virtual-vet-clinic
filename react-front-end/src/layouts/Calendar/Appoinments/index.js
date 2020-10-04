0e1edd5a6739c1cb6389072adf26509366ebf78b
TimaJuma <tima.xpl@gmail.com>
2020-10-03 18:21:58 -0600

    FE api/bookings

--- react-front-end/src/layouts/Calendar/Appoinments/index.js
+++ react-front-end/src/layouts/Calendar/Appoinments/index.js
@@ -72,12 +72,19 @@ export default function Appointment(props) {
   };
 
   useEffect(() => {
-    axios.get("/api/bookings", date).then((res) => {
-      const slots = hourExtracter(res.data.times);
-      setCurrentSlots([...slots]);
-      console.log("response on Date change", res.data);
-      console.log("CurrentSlots Updated", currentSlots);
-    });
+    const clinic_info = JSON.parse(localStorage.getItem("clinic_info"));
+    const clinic_id = clinic_info.id;
+    const date_str = date.getTime();
+    console.log("ID", clinic_id);
+    const data = { date };
+    axios
+      .post(`http://localhost:8080/api/bookings/${clinic_id}`, data)
+      .then((res) => {
+        // const slots = hourExtracter(res.data.times);
+        // setCurrentSlots([...slots]);
+        console.log("response on Date change", res.data);
+        // console.log("CurrentSlots Updated", currentSlots);
+      });
   }, [date]);
 
   return (
