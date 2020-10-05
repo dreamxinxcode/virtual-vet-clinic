5ae7361e02f241edbaa33401bb3248293ee877ca
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-10-04 20:28:15 -0700

    Signup logic

--- express-back-end/userRoutes.js
+++ express-back-end/userRoutes.js
@@ -2,22 +2,23 @@ const bcrypt = require("bcrypt");
 
 module.exports = function (router, database) {
   // Create a new user
-  // router.post("/", (req, res) => {
-  //   const user = req.body;
-  //   user.password = bcrypt.hashSync(user.password, 12);
-  //   console.log("password:", user.password);
-  //   database
-  //     .addUser(user)
-  //     .then((user) => {
-  //       if (!user) {
-  //         res.send({ error: "error" });
-  //         return;
-  //       }
-  //       req.session.userId = user.id;
-  //       res.send("🤗");
-  //     })
-  //     .catch((e) => res.send(e));
-  // });
+  router.post("/signup", (req, res) => {
+    const user = req.body;
+    // user.password = bcrypt.hashSync(user.password, 12);
+    // console.log("password:", user.password);
+    console.log("USER OBJ SIGNUP!", user);
+    database
+      .addUser(user)
+      .then(user => {
+        if (!user) {
+          res.send({ error: "error" });
+          return;
+        }
+        req.session.userId = user.id;
+        res.send("🤗");
+      })
+      .catch(e => res.send(e));
+  });
 
   /**
    * Check if a user exists with a given username and password
@@ -25,7 +26,7 @@ module.exports = function (router, database) {
    * @param {String} password encrypted
    */
   const login = function (email, type, password) {
-    return database.getUserWithEmail(email, type).then((user) => {
+    return database.getUserWithEmail(email, type).then(user => {
       // if (bcrypt.compareSync(password, user.password)) {
       //   return user;
       // }
@@ -38,7 +39,7 @@ module.exports = function (router, database) {
   router.post("/login", (req, res) => {
     const { email, password, type } = req.body;
     login(req.body.email, req.body.type, req.body.password)
-      .then((user) => {
+      .then(user => {
         if (!user) {
           res.send({ error: "error" });
           return;
@@ -51,14 +52,14 @@ module.exports = function (router, database) {
           type,
         });
       })
-      .catch((e) => res.send(e));
+      .catch(e => res.send(e));
   });
 
   router.post("/logout", (req, res) => {
     req.session.userId = null;
     req.session.userType = null;
     res.send({});
-    res.redirect('/');
+    res.redirect("/");
   });
 
   router.get("/me", (req, res) => {
@@ -70,7 +71,7 @@ module.exports = function (router, database) {
     }
     database
       .getUserWithId(userId, type)
-      .then((user) => {
+      .then(user => {
         if (!user) {
           res.send({ error: "no user with that id" });
           return;
@@ -81,7 +82,7 @@ module.exports = function (router, database) {
           type,
         });
       })
-      .catch((e) => res.send(e));
+      .catch(e => res.send(e));
   });
 
   return router;
